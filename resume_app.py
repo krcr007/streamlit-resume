ef86ea0f5442f66c091546a394a2b0b682be1a4c
krcr007 <106908370+krcr007@users.noreply.github.com>
2023-11-14 23:28:04 +0530

    Update resume_app.py

--- resume_app.py
+++ resume_app.py
@@ -1,40 +1,34 @@
 import streamlit as st
+from pdfminer.high_level import extract_text 
 from langchain.llms import GooglePalm
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-import fitz  # PyMuPDF
 
-# Initialize GooglePalm LLM
-api = "AIzaSyCE8GPxkKGibdVtSpL4SooR_7hS7auBzWI"
+# Initialize LangChain 
+api = "YOUR_API_KEY"
 llm = GooglePalm(google_api_key=api, temperature=0)
 
-# Function to extract text from PDF using PyMuPDF
+# PDF text extraction
 def extract_text_from_pdf(pdf_path):
-    doc = fitz.open(pdf_path)
-    text = ''
+  return extract_text(pdf_path)
 
-    for page_num in range(doc.page_count):
-        page = doc[page_num]
-        text += page.get_text()
-
-    doc.close()
-    return text
-
-# Function to generate analysis result
+# Generate analysis
 def generate_result(pdf_path):
-    st.info("Analyzing the resume... Please wait.")
-    prompt_template_resume = PromptTemplate(
-        input_variables=['text'],
-        template="Analyze the {text} resume and provide the job suited for it and also give ATS check and dont give all the details, just give the analysis. Also, don't give any salary expectations.  Also give strengths and weakness of the resume and suggest some changes.Give everything in detail",
-    )
-    model = LLMChain(llm=llm, prompt=prompt_template_resume)
-    resume_text = extract_text_from_pdf(pdf_path=pdf_path)
-    result = model.run({'text': resume_text})
-    return result
 
-# Streamlit app with custom CSS
+  prompt_template = PromptTemplate(template=
+  """
+  Analyze the {text} resume and provide the job suited for it and also give ATS check and dont give all the details, just give the analysis. Also, don't give any salary expectations.  
+  Also give strengths and weakness of the resume and suggest some changes.
+  Give everthing in detail
+  """)
+  
+  model = LLMChain(llm=llm, prompt=prompt_template)
+
+  resume_text = extract_text_from_pdf(pdf_path)
+  
+  return model.run({"text": resume_text})
+
 def main():
-    # Custom CSS for styling
     st.markdown(
         """
         <style>
@@ -51,23 +45,20 @@ def main():
         unsafe_allow_html=True
     )
 
-    st.title("📄 Resume Analysis App")
-    st.markdown("Upload a PDF file to get a personalized analysis of the resume.")
+  st.title("📄 Resume Analysis App")
 
-    uploaded_file = st.file_uploader("Choose a PDF file", type=["pdf"])
+  uploaded_file = st.file_uploader("Choose a PDF", type=["pdf"]) 
 
-    if uploaded_file is not None:
-        # Save the uploaded file to a known location
-        with open("uploaded_resume.pdf", "wb") as f:
-            f.write(uploaded_file.getvalue())
+  if uploaded_file:
+    with open("resume.pdf","wb") as f: 
+      f.write(uploaded_file.getvalue())
 
-        st.info("Analyzing the resume... Please wait.")
-        result = generate_result("uploaded_resume.pdf")  # Pass the saved path
+    result = generate_result("resume.pdf")
 
-        st.markdown("### 🚀 Analysis Result:")
-        st.write(result)
+    st.markdown("### 🚀 Analysis Result:")
+    st.write(result)
 
-        st.success("Analysis complete! Feel free to upload another resume.")
+    st.success("Analysis complete!")
 
 if __name__ == '__main__':
-    main()
+  main()
