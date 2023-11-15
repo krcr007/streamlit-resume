7d77679c879cfee47cfb88674ba20b7a61f43ba9
krcr007 <106908370+krcr007@users.noreply.github.com>
2023-11-15 12:36:36 +0530

    Update resume_app.py

--- resume_app.py
+++ resume_app.py
@@ -1,26 +1,26 @@
 import streamlit as st
 
-from pdfminer.high_level import extract_text  
+from pdfminer.high_level import extract_text
 
 from langchain.llms import GooglePalm
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
 # Initialize LangChain
-api = "AIzaSyCE8GPxkKGibdVtSpL4SooR_7hS7auBzWI" 
+api = "AIzaSyCE8GPxkKGibdVtSpL4SooR_7hS7auBzWI"
 llm = GooglePalm(google_api_key=api, temperature=0)
 
 # PDF text extraction
 def extract_text_from_pdf(pdf_path):
-  return extract_text(pdf_path)  
+  return extract_text(pdf_path)
 
 # Generate analysis
 def generate_result(pdf_path):
 
-  st.info("Analyzing the resume... Please wait.")
+    st.info("Analyzing the resume... Please wait.")
     prompt_template_resume = PromptTemplate(
         input_variables=['text'],
-        template="Analyze the {text} resume and provide the job suited for it and also give ATS check and dont give all the details, just give the analysis. Also, don't give any salary expectations.  Also give strengths and weakness of the resume and suggest some changes.Give everthing in detail",    )
+        template="Analyze the {text} resume and provide the job suited for it and also give ATS check and dont give all the details, just give the analysis. Also, don't give any salary expectations.  Also give strengths and weakness of the resume and suggest some changes.Give everthing in detail",)
     model = LLMChain(llm=llm, prompt=prompt_template_resume)
     resume_text = extract_text_from_pdf(pdf_path=pdf_path)
     result = model.run({'text': resume_text})
@@ -45,8 +45,8 @@ def main():
   uploaded_file = st.file_uploader("Choose a PDF", type=["pdf"])
 
   if uploaded_file:
-    with open("resume.pdf","wb") as f: 
-      f.write(uploaded_file.getvalue())  
+    with open("resume.pdf","wb") as f:
+      f.write(uploaded_file.getvalue())
 
     result = generate_result("resume.pdf")
 
