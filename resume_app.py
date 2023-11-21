04ebded26e22dd5cf3ba13d0b788680b9c3282c5
krcr007 <106908370+krcr007@users.noreply.github.com>
2023-11-21 09:18:01 +0530

    Update resume_app.py

--- resume_app.py
+++ resume_app.py
@@ -54,6 +54,86 @@ def main():
     st.write(result)
 
     st.success("Analysis complete!")
+import os
+import pandas as pd
+import streamlit as st
+import zipfile
+
+# ... (previous code)
+
+def analyze_resumes_in_folder(folder_path, job_role):
+    resumes = []
+    scores = []
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".pdf"):
+            resume_path = os.path.join(folder_path, filename)
+            resume_text = extract_text_from_pdf(pdf_path=resume_path)
+
+            # Generate analysis for each resume
+            result = model.run({'text': resume_text})
+
+            # Save resume and corresponding score
+            resumes.append(resume_text)
+            scores.append(result['ATS Score'])
+
+    # Create a DataFrame for easy sorting
+    df = pd.DataFrame({'Resume': resumes, 'ATS Score': scores})
+
+    # Sort resumes by ATS Score
+    df = df.sort_values(by='ATS Score', ascending=False).head(10)
+
+    return df
+
+def employer_section():
+    st.header("👔 Employer Section")
+
+    uploaded_folder = st.file_uploader("Upload a folder with resumes", type=["zip"], key="upload_folder")
+
+    if uploaded_folder:
+        with open("resumes.zip", "wb") as f:
+            f.write(uploaded_folder.getvalue())
+
+        job_role = st.text_input("Enter the job role you are looking for:")
+
+        if st.button("Analyze Resumes"):
+            # Extract the uploaded zip file
+            with zipfile.ZipFile("resumes.zip", "r") as zip_ref:
+                zip_ref.extractall("resumes_folder")
+
+            st.info("Analyzing resumes... Please wait.")
+            top_resumes_df = analyze_resumes_in_folder("resumes_folder", job_role)
+
+            st.markdown("### 🎯 Top 10 Resumes for the specified job role:")
+            st.table(top_resumes_df)
+
+def student_section():
+    st.header("📄 Resume Analysis App")
+
+    uploaded_file = st.file_uploader("Choose a PDF", type=["pdf"])
+
+    if uploaded_file:
+        with open("resume.pdf", "wb") as f:
+            f.write(uploaded_file.getvalue())
+
+        result = generate_result("resume.pdf")
+
+        st.markdown("### 🚀 Analysis Result:")
+        st.write(result)
+
+        st.success("Analysis complete!")
+
+def main():
+    st.sidebar.title("Switch Between Sections")
+    section = st.sidebar.radio("Select Section:", ["Student", "Employer"])
+
+    if section == "Student":
+        student_section()
+    elif section == "Employer":
+        employer_section()
 
 if __name__ == '__main__':
-  main()
+    main()
+
+
+
